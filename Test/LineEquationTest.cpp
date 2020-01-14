b6be3b1108cbc6e9e1017f0abf2c86e64ca595b1
Gaurav Singh <singhgaurav1302@gmail.com>
2020-01-14 17:08:07 +0100

    Line Equation Solver class

--- Test/LineEquationTest.cpp
+++ Test/LineEquationTest.cpp
@@ -1,5 +1,13 @@
 #include <gtest/gtest.h>
 
+#include "LineEquationSolver.h"
+
 TEST(LineEquationTest, GoogleTestSetup)
 {
 }
+
+TEST(LineEquationTest, InvokeLineEquation)
+{
+    LineEquationSolver eq;
+}
+
