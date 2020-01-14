e62473a2f128a5d50aff69d64c8d613762661b9a
Gaurav Singh <singhgaurav1302@gmail.com>
2020-01-14 17:16:34 +0100

    Implement getY() function

--- Source/include/LineEquationSolver.h
+++ Source/include/LineEquationSolver.h
@@ -5,4 +5,9 @@ public:
     LineEquationSolver(T x1_point, U y1_point, T x2_point, U y2_point)
     {
     }
+
+    long double getY(long double x)
+    {
+        return 0.0;
+    }
 };
--- Test/LineEquationTest.cpp
+++ Test/LineEquationTest.cpp
@@ -10,3 +10,9 @@ TEST(LineEquationTest, LineEquationSolver)
 {
     LineEquationSolver<int, int> eq(1, 2, 3, 4);
 }
+
+TEST(LineEquationTest, LineEquationSolverGetY)
+{
+    LineEquationSolver<int, int> eq(1, 2, 5, 6);
+    eq.getY(3);
+}
