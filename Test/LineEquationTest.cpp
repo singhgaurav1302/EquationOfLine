6457dff8dd8f8930cc53fc8637a89021f82283a7
Gaurav Singh <singhgaurav1302@gmail.com>
2020-01-15 10:38:16 +0100

    Line passing through axis:
    Get Y for different values of X

--- Test/LineEquationTest.cpp
+++ Test/LineEquationTest.cpp
@@ -2,8 +2,38 @@
 
 #include "LineEquationSolver.h"
 
-TEST(LineEquationTest, LineEquationSolverGetY)
+TEST(LineEquationTest, LinePassingThroughAxisGetYForXAs3)
 {
     LineEquationSolver<int, int> eq(1, 1, 5, 5);
     EXPECT_DOUBLE_EQ(3, eq.getY(3));
+}
+
+TEST(LineEquationTest, LinePassingThroughAxisGetYForXAs1)
+{
+    LineEquationSolver<int, int> eq(1, 1, 5, 5);
+    EXPECT_DOUBLE_EQ(1, eq.getY(1));
+}
+
+TEST(LineEquationTest, LinePassingThroughAxisGetYForXAs5)
+{
+    LineEquationSolver<int, int> eq(1, 1, 5, 5);
+    EXPECT_DOUBLE_EQ(5, eq.getY(5));
+}
+
+TEST(LineEquationTest, LinePassingThroughAxisGetYForXAs0)
+{
+    LineEquationSolver<int, int> eq(1, 1, 5, 5);
+    EXPECT_DOUBLE_EQ(0, eq.getY(0));
+}
+
+TEST(LineEquationTest, LinePassingThroughAxisGetYForXAs9)
+{
+    LineEquationSolver<int, int> eq(1, 1, 5, 5);
+    EXPECT_DOUBLE_EQ(9, eq.getY(9));
+}
+
+TEST(LineEquationTest, LinePassingThroughAxisGetYForXAsNeg9)
+{
+    LineEquationSolver<int, int> eq(1, 1, 5, 5);
+    EXPECT_DOUBLE_EQ(-9, eq.getY(-9));
 }
